9d0de6d6eb15e253bd186e24172959baa4f89ec5
Chris Owens <guitarrockstarco@gmail.com>
2020-09-18 08:36:50 -0400

    Able to display all information from single restaurant

--- models/restaurantsModel.js
+++ models/restaurantsModel.js
@@ -12,6 +12,26 @@ class Restaurants {
             return error
         }
     }
+
+    static async getReviewsByRestaurant(slug) {
+        try {
+            const response = await db.any(`SELECT * FROM reviews INNER JOIN restaurants ON reviews.restaurants_id = restaurants.id INNER JOIN reviewers ON reviews.reviewer_id = reviewers.id WHERE restaurants.slug = '${slug}';`);
+            return response;
+        } catch (error) {
+            console.error(error);
+            return error
+        }
+    }
+
+    static async getOne(slug) {
+        try {
+            const response = await db.one(`SELECT * FROM restaurants WHERE slug = '${slug}';`)
+            return response;
+        } catch (error) {
+            console.error(error);
+            return error
+        }
+    }
 }
 
 module.exports = Restaurants;
--- routes/business.js
+++ routes/business.js
@@ -5,22 +5,19 @@ const router = express.Router();
 const restaurantsModel = require('../models/restaurantsModel');
 
 
-router.get('/:name?', async (req, res) => {
-    const params = req.params;
-    console.log("Req params from business is:", params);
-    const restaurantData = await restaurantsModel.getAllRestaurants();
-    restaurantData.map(restaurant => {
-        if (restaurant.slug === req.params.name) {
-            res.render('template', {
-                locals: {
-                    title: restaurant.restaurant_name,
-                    name: restaurant.restaurant_name,
-                    category: restaurant.category
-                },
-                partials: {
-                    partial: 'partial-business'
-                }
-            })
+router.get('/:name', async (req, res) => {
+    const restaurantData = await restaurantsModel.getOne(req.params.name);
+    const reviewData = await restaurantsModel.getReviewsByRestaurant(req.params.name)
+    console.log(reviewData);
+    res.render('template', {
+        locals: {
+            title: restaurantData.restaurant_name,
+            name: restaurantData.restaurant_name,
+            category: restaurantData.category,
+            reviewData: reviewData
+        },
+        partials: {
+            partial: 'partial-business'
         }
     })
 
