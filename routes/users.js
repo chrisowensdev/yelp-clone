654cf24628c95bece0ffb31c05bf972aa4585dbe
Chris Owens <guitarrockstarco@gmail.com>
2020-09-21 12:29:20 -0400

    Add post routes

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,11 @@ const express = require('express');
 const router = express.Router();
 const users = require('../models/usersModel');
 
-router.get('/login', async (req, res) => {
-    const {
-        email,
-        password
-    } = req.body;
+router.get('/', (req, res) => {
+    res.redirect('/users/login')
+})
 
+router.get('/login', (req, res) => {
     res.render('template', {
         locals: {
             title: 'Login'
@@ -17,17 +16,10 @@ router.get('/login', async (req, res) => {
         partials: {
             partial: 'partial-login'
         }
-    })
-})
+    });
+});
 
-router.get('/signup', async (req, res) => {
-    const {
-        first_name,
-        last_name,
-        email,
-        password
-    } = req.body;
-
+router.get('/signup', (req, res) => {
     res.render('template', {
         locals: {
             title: 'Signup'
@@ -35,7 +27,27 @@ router.get('/signup', async (req, res) => {
         partials: {
             partial: 'partial-signup'
         }
-    })
+    });
+});
+
+router.post('/signup', async (req, res) => {
+    const {
+        first_name,
+        last_name,
+        email,
+        password
+    } = req.body;
+    console.log('Form submission is: ', req.body);
+    res.sendStatus(200);
+});
+
+router.post('/login', (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
+    console.log('Login form submission:', req.body);
+    res.sendStatus(200);
 })
 
 module.exports = router;
